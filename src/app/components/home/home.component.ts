ca87aeabfa6996aa42673e15239219ac093ebe92
Amitkanswal <amit.kanswal@contentstack.com>
2022-04-08 15:05:33 +0530

    feat:dynamic page genration [ECO-856]

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { ContentstackQueryService } from '../../cs.query.service';
 import { Meta } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { SeoService } from '../../seo.service';
 import { Store } from '@ngrx/store';
 import { actionBlogpost, actionPage } from 'src/app/store/actions/state.actions';
@@ -11,7 +12,7 @@ import { actionBlogpost, actionPage } from 'src/app/store/actions/state.actions'
   styleUrls: []
 })
 export class HomeComponent implements OnInit, AfterContentInit {
-  constructor(private cs: ContentstackQueryService, private metaTagService: Meta, private seo: SeoService, private store: Store) { }
+  constructor(private cs: ContentstackQueryService, private router: Router, private metaTagService: Meta, private seo: SeoService, private store: Store) { }
   page = 'Home';
   homeContent: any = {};
   entryUid: string;
@@ -37,19 +38,18 @@ export class HomeComponent implements OnInit, AfterContentInit {
     return inputObject;
   }
   getEntry() {
-    this.cs.getEntryWithQuery('page', { key: 'url', value: '/' },
+    this.cs.getEntryWithQuery('page', { key: 'url', value: this.router.url },
       ['page_components.from_blog.featured_blogs'],
       ["page_components.from_blog.featured_blogs.body",
         "page_components.section_with_buckets.buckets.description",]).then(entry => {
+          if (entry[0].length === 0) { this.router.navigate(["/404"])}
           this.homeContent = entry[0][0];
-
           const jsonData = this.filterObject(entry[0][0])
           this.store.dispatch(actionPage({ page: jsonData }));
           this.store.dispatch(actionBlogpost({ blogpost: null }));
           if (this.homeContent.seo) { this.seo.getSeoField(this.homeContent.seo, this.metaTagService); }
         }, err => {
           console.log(err);
-
         });
 
   }
